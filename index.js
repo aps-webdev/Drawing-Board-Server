29c9ba7af592e6b53a7a59037cdf8f13ed3d734f
aps-webdev <amanprataps31@gmail.com>
2024-01-02 23:38:48 +0530

    adding health check endpoint

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const router = express.Router();
+
+router.get("/health", (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: "Ok",
+    date: new Date(),
+  };
+
+  res.status(200).send(data);
+});
+
 const development = app.settings.env == "development";
 const clientUrl = development
   ? "http://localhost:3000"
   : "https://drawing-board-mur2f4uxn-aps-webdev.vercel.app/";
+
 const httpServer = createServer(app);
+
 const io = new Server(httpServer, {
   cors: {
     origin: clientUrl,
