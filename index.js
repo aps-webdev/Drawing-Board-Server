9255c7b713ca4e901bb7a3a75e14e25cbde70ed6
aps-webdev <amanprataps31@gmail.com>
2024-01-02 22:54:53 +0530

    emitting messgae and broadcast to open connections

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  console.log("Socket Server Connected", socket.id);
+  console.log("Socket Server Connected");
+  socket.on("beginPath", (args) => {
+    socket.broadcast.emit("beginPath", args);
+  });
+
+  socket.on("drawLine", (args) => {
+    socket.broadcast.emit("drawLine", args);
+  });
+
+  socket.on("configChange", (args) => {
+    socket.broadcast.emit("configChange", args);
+  });
 });
 
 httpServer.listen(5000);
